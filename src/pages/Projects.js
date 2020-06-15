fecc1e46bf07af4ea5a344b6ed07034a7fe19234
jason-nordheim <jason.nordheim@gmail.com>
2020-06-15 10:29:11 -0600

    added projects page, and placed project card on page... neeed to add more project cards with dynamically rendered content

---
+++ src/pages/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProjectCard from '../components/cards/ProjectCard'
+
+export default function Projects() {
+    return (
+        <div className="projects">
+            <h1>Projects</h1>
+            <ProjectCard /> 
+        </div>
+    )
+}
