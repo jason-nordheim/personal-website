4b1d73d65ed437aab3a9127d4acaf11a70e7d47b
jason-nordheim <jason.nordheim@gmail.com>
2020-06-15 10:32:28 -0600

    removed unneccessary code from index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import App from './app'
+import './app.css'
 
-import Navigation from './components/Navigation'
-
-const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <header>
-          <h1>Jason Nordheim</h1>
-          <Navigation />
-        </header>
-      </div>
-    </Router>
-  )
-}
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
     <App />
   </React.StrictMode>,
   document.getElementById('root')
