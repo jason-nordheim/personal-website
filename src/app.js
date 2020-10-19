98ab083cc37b39000bfbb6b47bfe996140a6595c
jason-nordheim <jason.nordheim@gmail.com>
2020-10-19 10:01:46 -0600

    removed all code from page components

--- src/app.js
+++ src/app.js
@@ -15,57 +15,7 @@ const PAGES = ["Home", "About", "Projects", "Contact"]
 
 
 const App = () => {
-  const [currentPage, setCurrentPage] = useState(0)
-
-  const switchPage = event => {
-    const text = event.target.innerText 
-    PAGES.forEach((page, index) => {
-      if (page.toUpperCase() === text.toUpperCase()){
-        setCurrentPage(index)
-      }
-    })
-  }
-
-  return (
-    <div className="App">
-      <header styles={styles.header}>
-        <Paper style={styles.nav} square elevation={2} >
-          <Grid container >
-            <Grid item sm={12}>
-                <Tabs indicatorColor="primary" 
-                      textColor="primary" 
-                      centered
-                      value={currentPage}
-                >
-                { PAGES.map(p => {
-                  return (
-                    <Grid key={p} item sm={Math.floor(12/PAGES.length)}>
-                      <Tab label={p} onClick={switchPage} />
-                    </Grid> 
-                  )
-                }) }
-              </Tabs>
-            </Grid>
-          </Grid>
-        </Paper>
-      </header>
-      <main styles={styles.main}>
-        <Grid container>
-          { 
-            currentPage === 0 
-              ? <Home /> 
-              : currentPage === 1 
-                ? <About /> 
-                : currentPage === 2 
-                  ? <Projects /> 
-                  : currentPage === 3 
-                    ? <Contact /> 
-                    : <Home />  
-          }
-        </Grid>
-    </main>
-    <footer>
-    </footer>
+    <div className="app">
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,24 +7,5 @@ import { goToUrl } from '../helpers'
 
 
 export default function Home() {
-    return (
-        <Grid item sm={12}>
-            <Grid container>
-                <Grid item sm={2}></Grid>
-                <Grid item sm={8}>
-                    <Paper style={styles.container}>
-                        <Typography variant={"h2"} >{data.contact.firstName} {data.contact.lastName}</Typography>
-                        <Typography variant={"body1"} >{data.home.subTitle}</Typography>
-                        <Grid item xs={12}>
-                            <a href="https://github.com/jason-nordheim">
-                                <GitHubIcon />
-                            </a>
-                        </Grid>
-
-                    </Paper>
-                </Grid>
-                <Grid item sm={2}></Grid>
-            </Grid>
-        </Grid>
-    )
+    return <div className="home"></div>
 }
