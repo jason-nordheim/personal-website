c9ea952587f68017e2f512f9115b10ae407cd125
jason-nordheim <jason.nordheim@gmail.com>
2020-11-15 21:49:08 -0700

    reset Component containers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,5 @@
-import React from 'react'
-import { Grid, Typography, Paper } from '@material-ui/core'
-import { data } from '../data/data'
-import { styles } from '../styles/main'
+import React from 'react'; 
 
+export const Home = () => <div className="about">About</div>
 
-export default function Home() {
-    return (
-        <Grid item sm={12}>
-            <Grid container>
-                <Grid item sm={2}></Grid>
-                <Grid item sm={8}>
-                    <Paper style={styles.container}>
-                        <Typography variant={"h2"} >{data.contact.firstName} {data.contact.lastName}</Typography>
-                        <Typography variant={"body1"} >{data.home.subTitle}</Typography>
-                        
-                    </Paper>
-                </Grid>
-                <Grid item sm={2}></Grid>
-            </Grid>
-        </Grid>
-    )
-}
+export default Home; 
