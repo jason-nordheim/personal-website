1b5a92a7c7184c68226f827831f19873772b9cfe
jason-nordheim <jason.nordheim@gmail.com>
2020-11-15 21:49:21 -0700

    simplified index component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app'
-import './app.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import App from "./app";
+import "./app.css";
 
-
-ReactDOM.render(
+const index = () => (
   <React.StrictMode>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
 
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <Router>
+      <Switch>
+        <App />
+      </Switch>
+    </Router>
+  </React.StrictMode>
+);
+
+ReactDOM.render(index, document.getElementById("root"));
