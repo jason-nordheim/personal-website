53e34ca2f37af7c0df9a0929409a1b870bd8eb18
jason-nordheim <jason.nordheim@gmail.com>
2020-11-15 21:36:15 -0700

    seperated data into distinct files

---
+++ src/data/education.js
@@ -0,0 +1,53 @@
+export const education = {
+  college: {
+    name: "Rochester Institute of Technology",
+    url: "https://www.rit.edu/",
+    logo:
+      "https://www.rit.edu/marketing/brandportal/images/717/toolkits/logo/RIT_seal.jpg",
+    address: {
+      street: "1 Lomb Memorial Drive",
+      city: "Rochester",
+      state: "NY",
+      zip: "14623",
+    },
+    major: {
+      title: "Management Information Systems",
+      degree: "Bachelor's of science",
+      url: "https://www.rit.edu/study/management-information-systems-mis-bs",
+    },
+    minors: [
+      {
+        name: "Web Development",
+        url: "https://www.rit.edu/study/web-development-minor",
+      },
+      {
+        name: "Digital Business",
+        url: "https://www.rit.edu/study/digital-business-minor",
+      },
+    ],
+    concentrations: ["Psychology"],
+    clubs: [{ name: "Lowenthal Service Group" }],
+    gpa: 3.49,
+    dates: {
+      start: {},
+      stop: {},
+    },
+  },
+  other: {
+    name: "Flatiron School",
+    program: "Software Engineering",
+    img:
+      "https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1397752768/e4ce735bc7e299313e5ca95eaf86a030.png",
+    url: "https://flatironschool.com/",
+    start: {
+      day: 30,
+      month: 3,
+      year: 2020,
+    },
+    end: {
+      day: 27,
+      month: 8,
+      year: 2020,
+    },
+  },
+};
---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+
+export const projects = [
+    {
+      title: "Snake", 
+      category: "games", 
+      technologies: ['JavaScript', "HTML", "CSS", "CSS Grid"], 
+      url: "https://github.com/jason-nordheim/pure-js-snake-wds", 
+    }, 
+    {
+      title: "Tetris", 
+      category: "games", 
+      technologies: ['React', "HTML", "CSS", "CSS Grid", "JavaScript", "JSX"], 
+      url: "https://github.com/jason-nordheim/react-tetris" 
+    }
+  ]
+  
