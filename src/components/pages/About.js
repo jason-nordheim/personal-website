158ea6f97392392ecea56da787bca97d0a837f26
jason-nordheim <jason.nordheim@gmail.com>
2020-11-18 13:43:51 -0700

    added styling to pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,7 @@
+import React from 'react'; 
+
+import './Pages.css'
+
+export const About = () => <div className="page about">About</div>
+
+export default About; 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from 'typewriter-effect'
+
+import './Pages.css'
+
+export const Home = () => {
+    const descriptors = ["Creator", "Software Engineer", "Web Developer", "Coder", "Problem-solver", "Geek"]
+    const speed = 35 
+  return (
+    <div className="page home">
+      <Typewriter className="cta" options={{
+            strings: descriptors, 
+            delay: speed, 
+            deleteSpeed: speed, 
+            autoStart: true, 
+            loop: true, 
+        }} />
+    </div>
+  );
+};
+
+export default Home;
