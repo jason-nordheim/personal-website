0b9b8bf1822187ac3697d19fa99dd67cadceb180
jason-nordheim <jason.nordheim@gmail.com>
2020-11-19 11:30:03 -0700

    typewriter functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Typewriter from "typewriter-effect"
-import { typewriterSpeed } from "../data/app"
+import Typewriter from "typewriter-effect";
+import { typewriterSpeed } from "../data/app";
 
 import "./NavBar.css";
 
@@ -20,61 +20,69 @@ export const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <div className="navbar-menu-icon" onClick={handleOpen}>
-          <i className={open ? "fas fa-times" : "fas fa-bars"} />
-        </div>
         <Link to="/" className="navbar-logo">
-           JN
+          JN
         </Link>
       </div>
       <div className="navbar-right">
+        <div className="navbar-menu-icon" onClick={handleOpen}>
+          <i className={open ? "fas fa-times" : "fas fa-bars"} />
+        </div>
         <ul className={open ? "navbar-menu active" : "navbar-menu"}>
           <li className="navbar-items">
-            <Link to="/" className="navbar-links" onClick={closeMenu}>
+            <Link to="/" className="navbar-links no-select" onClick={closeMenu}>
               <i className="fas fa-home" />
-              <Typewriter options={{
-                strings:"home",
-                delay: typeSpeed, 
-                cursor: '', 
-                autoStart: true,
-                loop: false
-            }}  /> 
+              <Typewriter
+                options={{
+                  strings: "home",
+                  delay: typeSpeed,
+                  cursor: "",
+                  autoStart: true,
+                  loop: false,
+                }}
+              />
             </Link>
           </li>
           <li className="navbar-items">
-            <Link to="/about" className="navbar-links" onClick={closeMenu}>
+            <Link to="/about" className="navbar-links no-select" onClick={closeMenu}>
               <i className="fas fa-child" />
-              <Typewriter options={{
-                strings:"about",
-                delay: typeSpeed, 
-                cursor: '', 
-                autoStart: true,
-                loop: false
-            }}  /> 
+              <Typewriter
+                options={{
+                  strings: "about",
+                  delay: typeSpeed,
+                  cursor: "",
+                  autoStart: true,
+                  loop: false,
+                }}
+              />
             </Link>
           </li>
           <li className="navbar-items">
-            <Link to="/projects" className="navbar-links" onClick={closeMenu}>
+            <Link to="/projects" className="navbar-links no-select" onClick={closeMenu}>
               <i className="fas fa-briefcase" />
-              <Typewriter options={{
-                strings:"projects",
-                delay: typeSpeed, 
-                cursor: '', 
-                autoStart: true,
-                loop: false
-            }}  /> 
+              <Typewriter
+                options={{
+                  strings: "projects",
+                  delay: typeSpeed,
+                  cursor: "",
+                  autoStart: true,
+                  loop: false,
+                }}
+              />
             </Link>
           </li>
           <li className="navbar-items">
-            <Link to="/contact" className="navbar-links" onClick={closeMenu}>
+            <Link to="/contact" className="navbar-links no-select" onClick={closeMenu}>
               <i className="fas fa-address-book" />
-              <Typewriter options={{
-                strings:"contact",
-                delay: typeSpeed, 
-                cursor: '', 
-                autoStart: true,
-                loop: false
-            }}  /> 
+              <Typewriter
+                options={{
+                  strings: "contact",
+                  delay: typeSpeed,
+                  cursor: "",
+                  autoStart: true,
+                  loop: false,
+                }}
+              />
             </Link>
           </li>
         </ul>
