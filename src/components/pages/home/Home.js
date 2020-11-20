f4ee1b6019e92f7fe7997aaed07dd8a10a5f5dea
jason-nordheim <jason.nordheim@gmail.com>
2020-11-19 18:25:18 -0700

    added take action component

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Hero } from './Hero'
 
+import { Hero } from "./Hero";
+import { TakeAction } from './TakeAction'
 
 export const Home = () => {
   return (
     <div className="home">
-      <section>
+      <section className="flex-center">
         <h1>Hello and Welcome!</h1>
-        <p>
-          Please allow me to introduce myself... 
-        </p>
+        <p>Please allow me to introduce myself...</p>
       </section>
       <section>
-        <Hero /> 
+        <Hero />
+      </section>
+      <section>
+        <TakeAction /> 
       </section>
     </div>
   );
