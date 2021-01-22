bdf380b3dccc4e0b027406194962b1e2374daed9
jason-nordheim <jason.nordheim@gmail.com>
2021-01-22 14:54:10 -0700

    prettier format

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -8,87 +8,122 @@ export default function socialMedia() {
   }
   return (
     <div className="social-media-div">
-
-      {socialMediaLinks.github ?
-        (<a href={socialMediaLinks.github} className="icon-button github" target="_blank" rel="noopener noreferrer">
+      {socialMediaLinks.github ? (
+        <a
+          href={socialMediaLinks.github}
+          className="icon-button github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-github"></i>
           <span></span>
-        </a>)
-        :
-        null}
+        </a>
+      ) : null}
 
-      {socialMediaLinks.linkedin ?
-        <a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank" rel="noopener noreferrer">
+      {socialMediaLinks.linkedin ? (
+        <a
+          href={socialMediaLinks.linkedin}
+          className="icon-button linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-linkedin-in"></i>
           <span></span>
         </a>
-        :
-        null}
+      ) : null}
 
-      {socialMediaLinks.gmail ?
-        <a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="_blank" rel="noopener noreferrer">
+      {socialMediaLinks.gmail ? (
+        <a
+          href={`mailto:${socialMediaLinks.gmail}`}
+          className="icon-button google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="far fa-envelope"></i>
           <span></span>
         </a>
-        :
-        null}
+      ) : null}
 
-      {socialMediaLinks.gitlab ?
-        <a href={socialMediaLinks.gitlab} className="icon-button gitlab" target="_blank" rel="noopener noreferrer">
+      {socialMediaLinks.gitlab ? (
+        <a
+          href={socialMediaLinks.gitlab}
+          className="icon-button gitlab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-gitlab"></i>
           <span></span>
         </a>
-        :
-        null}
+      ) : null}
 
-      {socialMediaLinks.facebook ?
-        <a href={socialMediaLinks.facebook} className="icon-button facebook" target="_blank" rel="noopener noreferrer">
+      {socialMediaLinks.facebook ? (
+        <a
+          href={socialMediaLinks.facebook}
+          className="icon-button facebook"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-facebook-f"></i>
           <span></span>
         </a>
-        :
-        null}
+      ) : null}
 
-      {socialMediaLinks.instagram ?
-        <a href={socialMediaLinks.instagram} className="icon-button instagram" target="_blank" rel="noopener noreferrer">
+      {socialMediaLinks.instagram ? (
+        <a
+          href={socialMediaLinks.instagram}
+          className="icon-button instagram"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-instagram"></i>
           <span></span>
         </a>
-        :
-        null}
+      ) : null}
 
-      {socialMediaLinks.twitter ?
-        <a href={socialMediaLinks.twitter} className="icon-button twitter" target="_blank" rel="noopener noreferrer">
+      {socialMediaLinks.twitter ? (
+        <a
+          href={socialMediaLinks.twitter}
+          className="icon-button twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-twitter"></i>
           <span></span>
         </a>
-        :
-        null}
+      ) : null}
 
-      {socialMediaLinks.medium ?
-        (<a href={socialMediaLinks.medium} className="icon-button medium" target="_blank">
+      {socialMediaLinks.medium ? (
+        <a
+          href={socialMediaLinks.medium}
+          className="icon-button medium"
+          target="_blank"
+        >
           <i className="fab fa-medium"></i>
           <span></span>
-        </a>)
-        :
-        null}
+        </a>
+      ) : null}
 
-      {socialMediaLinks.devTo ?
-        (<a href={socialMediaLinks.devTo} className="icon-button dev-to" target="_blank">
+      {socialMediaLinks.devTo ? (
+        <a
+          href={socialMediaLinks.devTo}
+          className="icon-button dev-to"
+          target="_blank"
+        >
           <i className="fab fa-dev"></i>
           <span></span>
-        </a>)
-        :
-        null}
+        </a>
+      ) : null}
 
-      {socialMediaLinks.stackoverflow ?
-        (<a href={socialMediaLinks.stackoverflow} className="icon-button stack-overflow" target="_blank">
+      {socialMediaLinks.stackoverflow ? (
+        <a
+          href={socialMediaLinks.stackoverflow}
+          className="icon-button stack-overflow"
+          target="_blank"
+        >
           <i className="fab fa-stack-overflow"></i>
           <span></span>
-        </a>)
-        :
-        null}
-
+        </a>
+      ) : null}
     </div>
   );
 }
