16d4a089ae7feddac8b931dd0e3d0f411ec55740
jason-nordheim <jason.nordheim@gmail.com>
2021-01-21 17:03:11 -0700

    changed 'Work Experience' to 'Experience'

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -46,7 +46,7 @@ function Header() {
           )}
           {viewExperience && (
             <li>
-              <a href="#experience">Work Experiences</a>
+              <a href="#experience">Experience</a>
             </li>
           )}
           {viewOpenSource && (
