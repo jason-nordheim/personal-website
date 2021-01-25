4d75637a91c7e22c636f6717c0551154486bdeff
jason-nordheim <jason.nordheim@gmail.com>
2021-01-25 13:19:08 -0700

    added sort function so that skills are listed alphabetically

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -7,14 +7,22 @@ export default function SoftwareSkill() {
     <div>
       <div className="software-skills-main-div">
         <ul className="dev-icons">
-          {skillsSection.softwareSkills.map((skills,i) => {
-            return (
-              <li key={i} className="software-skill-inline" name={skills.skillName}>
-                <i className={skills.fontAwesomeClassname}></i>
-                <p>{skills.skillName}</p>
-              </li>
-            );
-          })}
+          {skillsSection.softwareSkills
+            .sort((skillA, skillB) => {
+              return skillA.skillName > skillB.skillName ? 1 : -1; // sort alphabetically
+            })
+            .map((skills, i) => {
+              return (
+                <li
+                  key={i}
+                  className="software-skill-inline"
+                  name={skills.skillName}
+                >
+                  <i className={skills.fontAwesomeClassname}></i>
+                  <p>{skills.skillName}</p>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
